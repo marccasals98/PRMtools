0986e77d87ecebb0a9915374b53c54581fb627c9
marccasals98 <marc.casals8@gmail.com>
2022-01-31 13:33:22 +0100

    Fixed 2 bugs and added documentation

--- R/filt_data.R
+++ R/filt_data.R
@@ -4,17 +4,29 @@
 #' that max_times. For example, in a poll we descart people who said 'Ns/Nr' more
 #' than 12 times.
 #'
+#'
+#'
 #' @param df the Dataframe that you introduced from the function haven::read_sav()
 #' @param variable_list the variables that we want to filter.
 #' @param max_times The number of maximum times that a code can appear.
 #' @param codes_list The array of codes that we want to filter.
 #'
 #' @return returns the df without these rows.
-#' @examples filt_data(df=data_frame, variable_list= c("p15_1", "p15_2"), max_times=7, codes_list=c(97,98,99))
+#' @examples
+#' #Basic usage
+#' p15_1 <- c(0, 2, 3, 99, 99, 99, 2)
+#' p15_2 <- c(1, 2, 5, 3, 4, 52, 2)
+#' data_frame <- data.frame(p15_1, p15_2)
+#' data_frame_output <- filt_data(
+#'   df = data_frame,
+#'   variables_list = c ("p15_1", "p15_2"),
+#'   max_times =  2,
+#'   codes_list =  c(97, 98, 99)
+#' )
+#'
 #' @export
 
 filt_data <- function(df, variables_list, max_times, codes_list) {
-  colnames(df) <- tolower(colnames(df))
   times <- apply(
     X = df[, variables_list],
     MARGIN = 1,
@@ -23,7 +35,7 @@ filt_data <- function(df, variables_list, max_times, codes_list) {
     }
   )
   to_descart <- rep(0, nrow(df))
-  to_descart[times > 7] <- 1
+  to_descart[times > max_times] <- 1
   to_descart_logical <- (to_descart == 0)
   df <- df[to_descart_logical, ]
   return(df)
